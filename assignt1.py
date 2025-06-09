b95b9d279348c434adfb1fd53e0a3639ae65c410
Pratiksha Satapathy <pratikshasatapathy1@gmail.com>
2025-06-09 16:16:01 +0530

    assignment 1

---
+++ assignt1.py
@@ -0,0 +1,12 @@
+a=input('a:')
+a=int(a)
+b=input('b: ')
+b=int(b)
+c=a+b
+print('the sum of the numbers is: ',c)
+d=a-b
+print('the difference of the numbers is : ',d)
+e=a*b
+print('the product of the number is : ',e)
+f=a/b
+print('the quotient of the number is: ',f)
